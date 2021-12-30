4865f2178c9741ffa2f5d0a48eab4df5766acbb4
Mandy Tsang <mandy.tsang007@gmail.com>
2021-12-29 20:45:59 -0800

    initial, set up code quiz questions

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+/*pseudocode
+create a counter to record score and initially set its value to zero
+create a start button
+create a timer
+create an array in which includes quiz questions
+when the user presses on the button, it set off the timer
+once the timer starts, a prompt will be displayed to ask the first question
+after the user picks an answer and presses submit, the program will examine the answer
+examine if the answer is answered correctly 
+if true (when answer correctly)
+    - display "Right"
+    -increment the counter by 1
+    - move to the next question. 
+if false (when answer incorrectly), 
+    - display "Wrong"
+    - subtract time from the clock
+use for loop to move through each question one by one
+when all questions are answered or when the time ran out, the game comes to an end. 
+display a box asking user to input his/her initial
+when user clicks submit
+display highscores
+
+
+
+
+
+
+*/
